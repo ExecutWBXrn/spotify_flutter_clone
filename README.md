ab2b5cbaec2bccd10b3f5f4529bceeaff1f35836
Roman Beniuk <executwbxrn@gmail.com>
2026-02-20 16:37:10 +0000

    ref: routes

--- README.md
+++ README.md
@@ -4,13 +4,11 @@ A new Flutter project.
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+Point of this project was exploring widgets, no more, I have tried here to replicate spotify UI
 
-A few resources to get you started if this is your first Flutter project:
+![img_4.png](img_4.png)
+![img_3.png](img_3.png)
+![img.png](img.png)
+![img_1.png](img_1.png)
+![img_2.png](img_2.png)
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
--- lib/main.dart
+++ lib/main.dart
@@ -3,83 +3,116 @@ import 'package:spotify/pages/home.dart';
 import 'package:spotify/pages/search.dart';
 import 'pages/mylibra.dart';
 import 'package:spotify/pages/premium.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
-  runApp(HomePage());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final GoRouter _router;
+
+  final String initialLocation = '/home';
+
+  final _shellKey = GlobalKey<NavigatorState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _router = GoRouter(
+      initialLocation: initialLocation,
+      routes: [
+        ShellRoute(
+          navigatorKey: _shellKey,
+          builder: (context, state, child) {
+            return HomePage(child: child);
+          },
+          routes: [
+            GoRoute(path: '/home', builder: (context, state) => HOME_PAGE()),
+            GoRoute(
+              path: '/search',
+              builder: (context, state) => SEARCH_PAGE(),
+            ),
+            GoRoute(
+              path: '/library',
+              builder: (context, state) => MY_LIBRARY(),
+            ),
+            GoRoute(path: '/premium', builder: (context, state) => PREMIUM()),
+          ],
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return HomePage();
+    return MaterialApp.router(theme: ThemeData.dark(), routerConfig: _router);
   }
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return HomePageState();
+class HomePage extends StatelessWidget {
+  final Widget child;
+
+  const HomePage({super.key, required this.child});
+
+  int _calculateSelectedIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).uri.toString();
+    if (location.startsWith('/home')) {
+      return 0;
+    }
+    if (location.startsWith('/search')) {
+      return 1;
+    }
+    if (location.startsWith('/library')) {
+      return 2;
+    }
+    if (location.startsWith('/premium')) {
+      return 3;
+    }
+    return 0;
   }
-}
 
-class HomePageState extends State<HomePage> {
-  int _currentIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
+  void _onItemTapped(int index, BuildContext context) {
+    switch (index) {
+      case 0:
+        context.go('/home');
+        break;
+      case 1:
+        context.go('/search');
+        break;
+      case 2:
+        context.go('/library');
+        break;
+      case 3:
+        context.go('/premium');
+        break;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark(),
-      home: Scaffold(
-        body: IndexedStack(
-          index: _currentIndex,
-          children: [HOME_PAGE(), SEARCH_PAGE(), MY_LIBRARY(), PREMIUM()],
-        ),
-        extendBody: true,
-        bottomNavigationBar: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.black12, Colors.black],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ),
+    return Scaffold(
+      body: child,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _calculateSelectedIndex(context),
+        onTap: (index) => _onItemTapped(index, context),
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_music),
+            label: 'Library',
           ),
-          child: BottomNavigationBar(
-            backgroundColor: Colors.transparent,
-            currentIndex: _currentIndex,
-            selectedItemColor: Colors.white,
-            unselectedItemColor: Colors.grey,
-            iconSize: 30,
-            selectedLabelStyle: TextStyle(
-              fontSize: 10,
-              fontWeight: FontWeight.w800,
-            ),
-            unselectedLabelStyle: TextStyle(
-              fontSize: 10,
-              fontWeight: FontWeight.w800,
-            ),
-            onTap: _onItemTapped,
-            type: BottomNavigationBarType.fixed,
-            items: <BottomNavigationBarItem>[
-              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Головна"),
-              BottomNavigationBarItem(icon: Icon(Icons.search), label: "Пошук"),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.library_books),
-                label: "Моя бібліотека",
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.contactless_sharp),
-                label: "Premium",
-              ),
-              BottomNavigationBarItem(icon: Icon(Icons.add), label: "Створити"),
-            ],
-          ),
-        ),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Premium'),
+        ],
       ),
     );
   }
